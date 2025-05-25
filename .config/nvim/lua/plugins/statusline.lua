4c6c35876d3bc5eaa4f9da175517df72758deaa6
chiinthedesert <176766221+chiinthedesert@users.noreply.github.com>
2025-05-25 23:52:42 +0700

    Configure neovim again for better understanding

---
+++ .config/nvim/lua/plugins/statusline.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- optional, for icons
+    },
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "kanagawa"
+        }
+      }
+    end
+  },
+}
+
