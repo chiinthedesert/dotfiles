3e772f98f095803f5682557b932006e0c24365eb
chiinthedesert <176766221+chiinthedesert@users.noreply.github.com>
2025-05-26 14:59:46 +0700

    Add fzflua, treesitter and which-key

---
+++ .config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,12 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  keys = {
+    { "<leader>ff", function() require('fzf-lua').files() end, desc="find files in current working dir"},
+    { "<leader>fg", function() require('fzf-lua').live_grep() end, desc="find by grepping in current working dir"}
+  }
+}
