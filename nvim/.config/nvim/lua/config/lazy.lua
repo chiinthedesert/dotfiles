0cb1aa83aec24d9b8642fece8cb93104b27468b8
chiinthedesert <176766221+chiinthedesert@users.noreply.github.com>
2026-01-29 15:41:03 +0700

    again with nvim and kanata

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -25,22 +25,5 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true, notify = false },
-  ui = {
-    icons = {
-      cmd = "⌘",
-      config = "🛠️",
-      event = "📅",
-      ft = "📂",
-      init = "⚙️",
-      keys = "🗝️",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "💾",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-      lazy = "💤 ",
-    },
-  }
+  checker = { enabled = true },
 })
