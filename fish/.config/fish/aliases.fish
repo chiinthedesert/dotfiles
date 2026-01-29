7a0d6761ed0a6df2d35fc58adae87796d9288691
chiinthedesert <176766221+chiinthedesert@users.noreply.github.com>
2026-01-29 12:02:13 +0700

    change all structure

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,15 @@
+alias l. 'ls -d .*'   # List only hidden files (dotfiles)
+alias la 'ls -a'
+alias ls 'ls -l'
+alias c 'clear'
+
+
+alias cp 'cp -i'                   # Confirm before overwriting a file
+alias mv 'mv -i'                   # Confirm before overwriting
+alias rm 'rm -i'                   # Confirm before deleting
+alias .. 'cd ..'
+
+alias tr 'trash-put'
+alias trr 'trash-restore'
+alias tre 'trash-empty'
+alias trl 'trash-list'
