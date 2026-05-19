25f066fbdf0b3331a6ab01a59bb894414fedb0eb
chiinthedesert <176766221+chiinthedesert@users.noreply.github.com>
2026-05-19 23:44:15 +0700

    fish

--- fish/.config/fish/conf.d/you_should_use.fish
+++ fish/.config/fish/conf.d/you_should_use.fish
@@ -6,7 +6,7 @@ __ysu__set_default RED (set_color red)
 __ysu__set_default YELLOW (set_color yellow)
 __ysu__set_default VIOLET (set_color magenta)
 
-set --global YSU__MESSAGE_POSITION "before"
+set --global YSU__MESSAGE_POSITION "after"
 set --global YSU__HARDCORE_MODE false
 
 functions -q __ysu__check_aliases
